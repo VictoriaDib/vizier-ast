f846ee775696983781f7ee9b0d368d1dd72c7ca4
Shawn z <zimmermannshawn@gmail.com>
2023-06-14 14:12:54 -0400

    initial functiondef

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ from typing import Any, Iterator
 
 
 class Cell_Scope:
-    INSIDE = "inside"
-    OUTSIDE= "outside"
-    EITHER = "either"
+    INSIDE  = "inside"
+    OUTSIDE = "outside"
+    EITHER  = "either"
 
 class Visit_AST(ast.NodeVisitor, Cell_Scope):
     def __init__(self):
@@ -20,35 +20,48 @@ class Visit_AST(ast.NodeVisitor, Cell_Scope):
         self.scope_stack.appendleft({})
     
     def visit_FunctionDef(self, node: FunctionDef) -> Any:
-        ## Need to deal with transitive d3eps here somehow
-        return super().generic_visit(node)
+        ## Add function to current stack
+        self.scope_stack[0][node.name] = (self.INSIDE, []) 
+        ## Add a new scope for the function vars
+        self.scope_stack.appendleft({}) 
+        ## Visit the body
+        super().generic_visit(node)
+
+        self.scope_stack.popleft()  
 
     def visit_For(self, node: For) -> Any:
         ## If it enters a new scope, make a new scope with the current node
-        scope = {node.target.id: (self.INSIDE, [])}
+        scope = {node.target.id: self.INSIDE}
         self.scope_stack.appendleft(scope)
         super().generic_visit(node)
         self.scope_stack.popleft()
 
     def visit_AsyncFor(self,node: AsyncFor) -> Any:
-        scope = {node.target.id: (self.INSIDE, [])}
+        scope = {node.target.id: self.INSIDE}
         self.scope_stack.appendleft(scope)
         super().generic_visit(node)
         self.scope_stack.popleft()
     
     def visit_Assign(self, node: Assign) -> Any: ## NOT DONE
         ## Since append left puts it in the front, accessing the 0th element gets top o' stack
-        self.scope_stack[0][node.targets[0].id] = (self.OUTSIDE, []) # Targets could be a list so we'd have to iterate over this
+        self.scope_stack[0][node.targets[0].id] = self.OUTSIDE # Targets could be a list so we'd have to iterate over this
+
+        ## If we get something in a function that is declared outside the scope of the function add it to deps
+        if isinstance(node.value, ast.Name) and (node.value.id not in self.scope_stack[0]) and (node.value.id in self.scope_stack[1]):
+            for name in self.scope_stack[1]:
+                if isinstance(self.scope_stack[1][name], tuple):
+                    self.scope_stack[1][name][1].append(node.value.id)
         super().generic_visit(node)
 
     def visit_AugAssign(self, node: AugAssign) -> Any:
-        self.scope_stack[0][node.target.id] = (self.OUTSIDE, [])
+        self.scope_stack[0][node.target.id] = self.OUTSIDE
         super().generic_visit(node)
 
     def visit_AnnAssign(self, node: AnnAssign) -> Any:
-        self.scope_stack[0][node.target.id] = (self.OUTSIDE, [])
+        self.scope_stack[0][node.target.id] = self.OUTSIDE
         super().generic_visit(node)
 
+    # Make this into a function of some sort ?? #
     def visit_Name(self, node: Name) -> Any:
         if isinstance(node.ctx, ast.Store) :
             ## Using this because what if we're curreingly in a scope and use a var defined in another scope
@@ -61,12 +74,12 @@ def main():
     with open("example2.py", "r") as source:
         tree = ast.parse(source.read())
 
-    # print(ast.dump(tree, indent=4))
+    print(ast.dump(tree, indent=4))
     vis = Visit_AST()
     vis.visit(tree)
     # print("Scope: ", vis.scope_stack)
     # print("store: ", vis.main_dict_store)
-    print("Outside Reads:  ", vis.outside_reads)
+    # print("Outside Reads:  ", vis.outside_reads)
 
             
         
